375ed5b3805f80aa7be5c40c0a98495ee453843d
taha7 <taha.mohamed4213@gmail.com>
2019-06-27 18:12:46 +0200

    Test that slots are listed in chronological order

--- tests/Feature/SponsorShipTest.php
+++ tests/Feature/SponsorShipTest.php
@@ -8,29 +8,33 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Sponsorable;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use App\SponsorableSlot;
+use Carbon\Carbon;
 
 class SponsorShipTest extends TestCase
 {
     /** @test */
-    public function view_new_sponsorship_page()
+    public function sponsorable_slots_are_listed_in_chronological_order()
     {
         $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
 
         $sponsorableSlots = new EloquentCollection([
-            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]),
-            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]),
-            factory(SponsorableSlot::class)->create(['sponsorable_id' => $sponsorable]),
+            $slotA = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(10), 'sponsorable_id' => $sponsorable]),
+            $slotB = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(30), 'sponsorable_id' => $sponsorable]),
+            $slotC = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(5), 'sponsorable_id' => $sponsorable]),
         ]);
 
         $response = $this->get('full-stack-radio/sponsorships/new');
 
         $response->assertSuccessful();
 
-
         $this->assertCount(3, $response->data('sponsorableSlots'));
 
         $this->assertTrue($sponsorable->is($response->data('sponsorable')));
 
-        $sponsorableSlots->assertEquals($response->data('sponsorableSlots'));
+        $this->assertTrue($slotC->is($response->data('sponsorableSlots')[0]));
+        $this->assertTrue($slotA->is($response->data('sponsorableSlots')[1]));
+        $this->assertTrue($slotB->is($response->data('sponsorableSlots')[2]));
+
+        // $sponsorableSlots->assertEquals($response->data('sponsorableSlots'));
     }
 }
